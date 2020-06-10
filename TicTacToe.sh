a1157a392f5012b23928a331c9735c970a699909
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-10 22:19:13 +0530

    Added feature to select symbol

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -12,13 +12,35 @@ function resetBoard(){
 }
 
 function toss(){
+	firstPlayFlag=0
 	tossRes=$((RANDOM%2))
 	if [ $tossRes -eq 1 ]
 	then
+		firstPlayFlag=1
 		echo "Computer Won Toss"
+		symbol=$((RANDOM%2))
+                if [ $symbol -eq 1 ]
+                then
+                        compSymbol='X'
+                        userSymbol='O'
+                else
+                        compSymbol='O'
+                        userSymbol='X'
+                fi
 	else
+		firstPlayFlag=2
 		echo "User won toss"
+		read -p "Enter 1 for 'X' symbol and 2 for 'O' symbol: " symbol
+                if [ $symbol -eq 1 ]
+                then
+                        userSymbol='X'
+                        compSymbol='O'
+                else
+                        userSymbol='O'
+                        compSymbol='X'
+                fi
 	fi
+	echo " ";echo "Assigned Symbols";echo "Computer: $compSymbol";echo "User: $userSymbol"
 }
 
 resetBoard

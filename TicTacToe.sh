9e775b95c0e090ca401a4362c26a68d698bf8ca8
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-10 19:10:21 +0530

    start fresh with resetting board (uc1)

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,30 @@
-#!/bin/bash -x
+##!/bin/bash -x
 
-echo "O X O X O X Welcome to tic tac toe X O X O X O"
+echo "<-+-+-+-Welcome to Tic Tac Toe-+-+-+->"
+
+declare -a positions
+function resetBoard(){
+	for ((i=0; i<=9; i++))
+	do
+		position[$i]=$i
+	done
+}
+
+function board(){
+	printf "+-----+-----+-----+\n"
+	printf "|     |     |     |\n"
+	printf "|  ${position[1]}  |  ${position[2]}  |  ${position[3]}  |\n"
+	printf "|     |     |     |\n"
+	printf "+-----+-----+-----+\n"
+        printf "|     |     |     |\n"
+	printf "|  ${position[4]}  |  ${position[5]}  |  ${position[6]}  |\n"
+        printf "|     |     |     |\n"
+        printf "+-----+-----+-----+\n"
+        printf "|     |     |     |\n"
+	printf "|  ${position[7]}   |  ${position[8]}  |  ${position[9]}  |\n"
+        printf "|     |     |     |\n"
+        printf "+-----+-----+-----+"
+}
+
+resetBoard
+board

4258c809c5943d1f3bb260f14b60e34aae6268de
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-10 21:57:51 +0530

    Added to Reset the Board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -8,23 +8,7 @@ function resetBoard(){
 	do
 		position[$i]=$i
 	done
-}
-
-function board(){
-	printf "+-----+-----+-----+\n"
-	printf "|     |     |     |\n"
-	printf "|  ${position[1]}  |  ${position[2]}  |  ${position[3]}  |\n"
-	printf "|     |     |     |\n"
-	printf "+-----+-----+-----+\n"
-        printf "|     |     |     |\n"
-	printf "|  ${position[4]}  |  ${position[5]}  |  ${position[6]}  |\n"
-        printf "|     |     |     |\n"
-        printf "+-----+-----+-----+\n"
-        printf "|     |     |     |\n"
-	printf "|  ${position[7]}   |  ${position[8]}  |  ${position[9]}  |\n"
-        printf "|     |     |     |\n"
-        printf "+-----+-----+-----+"
+	echo -n "Board Reseted !"
 }
 
 resetBoard
-board

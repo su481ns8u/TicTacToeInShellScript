39dcc2100a09ac9d010799b949f21fe69e642cd2
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-15 09:14:42 +0530

    REFACTORED code to make more functional ADDED alternate function for checking winner

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printf "<--+--+--+--Welcome To Tic Tac Toe--+--+--+-->\n"
+
 #ARRAY DECLARATION
 declare -A positions
 
@@ -64,7 +66,7 @@ function toss(){
 	case $tossResult in
 		$TOSS_WIN)
 			currentTurn=$COMP_TURN
-			printf "\nComputer Won Toss !"
+			printf "\nComputer Won Toss !\n"
 			compChoice=$((RANDOM%TOSS_LIM))
 			case $compChoice in
 				$TOSS_WIN)
@@ -97,7 +99,7 @@ function toss(){
 						flag=1
 						;;
 					*)
-						printf "Enter the valid choice !"
+						printf "\nEnter the valid choice !"
 						flag=0
 						;;
 				esac
@@ -113,7 +115,7 @@ function userTurn() {
     read -p "Enter position you want to add $userSymbol: " choice
     while [[ ${positions[$choice]} != $choice ]]
     do
-        printf "\nPlace already taken chose another one\n"
+        printf "\n\nPlace already taken chose another one\n"
 		read -p "Enter position you want to add $userSymbol: " choice
     done
     positions[$choice]=$userSymbol
@@ -179,7 +181,7 @@ function canCorner(){
 			continue
 		elif [[ ${positions[$i]} == $i ]]
 		then
-			printf "Computer Chose: $i"
+			printf "\nComputer Chose: $i"
 			position=$MOVE
 			positions[$i]=$compSymbol
 			break
@@ -193,7 +195,7 @@ function canCenter(){
 	then
 		position=$MOVE
 		$positions[$CENTER]=$compSymbol
-		printf "Computer chose: $i"
+		printf "\nComputer chose: $i"
 	fi
 }
 
@@ -237,7 +239,7 @@ function checkWin(){
 		whoWon
     elif [[ ${positions[1]} != 1 && ${positions[2]} != 2 && ${positions[3]} != 3 && ${positions[4]} != 4 && ${positions[5]} != 5 && ${positions[6]} != 6 && ${positions[7]} != 7 && ${positions[8]} != 8 && ${positions[9]} != 9 ]]
     then
-        printf "\n\nThe game is tie !!!"
+        printf "\n\nThe game is tie !!!\n"
         endGameFlag=1
     fi
 }
@@ -263,8 +265,44 @@ function leaderBoard(){
 	printf "\n+------------+------------+\n"
 }
 
-#FUNCTION TO START PLAYING GAME
-function playGame() {
+#FUNCTION TO PLAY GAME
+function playGame(){
+	resetBoard
+	toss
+	compWinFlag=$LOSE
+	userWinFlag=$LOSE
+	endGameFlag=$START
+	while [[ $endGameFlag != $END ]]
+	do
+		displayBoard
+		if [[ $currentTurn == $COMP_TURN ]]
+		then
+			computerTurn
+			checkWin
+		if [[ $endGameFlag == $END && $compWinFlag == $WIN ]]
+		then
+			printf "\n\nComputer Won !!!\n"
+			computerScore=$((computerScore+1))
+			break
+		fi
+		currentTurn=$USER_TURN
+		elif [[ $currentTurn == $USER_TURN ]]
+		then
+			userTurn
+			checkWin
+			if [[ $endGameFlag == $END && $userWinFlag == $WIN ]]
+			then
+				printf "\n\nUser Won !!!\n"
+				userScore=$((userScore+1))
+				break
+			fi
+			currentTurn=$COMP_TURN
+		fi
+	done
+}
+
+#MAIN
+function main() {
 	local flag=0
 	while [[ $flag != 1 ]]
 	do
@@ -272,44 +310,14 @@ function playGame() {
 		read -p "Choice: " choice
 		case $choice in
 			1)
-				resetBoard
-				toss
-				compWinFlag=$LOSE
-				userWinFlag=$LOSE
-				endGameFlag=$START
-				while [[ $endGameFlag != $END ]]
-					do
-					displayBoard
-					if [[ $currentTurn == $COMP_TURN ]]
-					then
-						computerTurn
-						checkWin
-						if [[ $endGameFlag == $END && $compWinFlag == $WIN ]]
-						then
-							printf "\n\nComputer Won !!!"
-							computerScore=$((computerScore+1))
-							break
-						fi
-						currentTurn=$USER_TURN
-					elif [[ $currentTurn == $USER_TURN ]]
-					then
-						userTurn
-						checkWin
-						if [[ $endGameFlag == $END && $userWinFlag == $WIN ]]
-						then
-							printf "\n\nUser Won !!!"
-							userScore=$((userScore+1))
-							break
-						fi
-						currentTurn=$COMP_TURN
-					fi
-				done
+				playGame
 				;;
 			2)
 				leaderBoard
 				;;
 			3)
 				flag=1
+				printf "\n**** Thankyou for playing ****"
 				;;
 			*)
 				printf "\nNot a valid choice !!!"
@@ -317,5 +325,42 @@ function playGame() {
 	done
 }
 
-#PLAY GAME HERE
-playGame
+#CALL MAIN
+main
+
+####	ALTERNATIVE FOR CHECK WINNER ####
+
+# function checkWin(){
+# 	local chFlag=0
+# 	for ((i=1; i<=9; i++))
+# 	do
+# 		if [[ $i == 1 || $i == 4 || $i == 7 ]]
+# 		then
+# 			j=$(($i+1))
+# 			k=$(($i+2))
+# 			if [[ ${positions[$i]} == ${positions[$j]} || ${positions[$i]} == ${positions[$k]} ]]
+# 			then 
+# 				whoWon
+# 			fi
+# 		elif [[ $i -le 3 ]]
+# 		then
+# 			j=$(($i+3))
+# 			k=$(($i+6))
+# 			if [[ ${positions[$i]} == ${positions[$k]} || ${positions[$i]} == ${positions[$k]} ]]
+# 			then 
+# 				whoWon
+# 			fi
+# 		fi
+# 	done
+# 	if [[ ${positions[1]} == ${positions[5]} && ${positions[5]} == ${positions[9]} ]]
+#     then
+# 		whoWon
+#     elif [[ ${positions[7]} == ${positions[5]} && ${positions[5]} == ${positions[3]} ]]
+#     then
+# 		whoWon
+#     elif [[ ${positions[1]} != 1 && ${positions[2]} != 2 && ${positions[3]} != 3 && ${positions[4]} != 4 && ${positions[5]} != 5 && ${positions[6]} != 6 && ${positions[7]} != 7 && ${positions[8]} != 8 && ${positions[9]} != 9 ]]
+#     then
+#         printf "\n\nThe game is tie !!!"
+#         endGameFlag=1
+#     fi
+# }
